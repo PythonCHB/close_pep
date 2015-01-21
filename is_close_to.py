00c8835ecec0690e7889302fb7d16cfffeb6d30d
chris.barker <chris.barker@noaa.gov>
2015-01-21 15:28:50 -0800

    updated to include "asymetric" approach

---
+++ is_close_to.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+An implimentaion for an is_close_to() function, for possible inclusion in
+the Python standrad library.
+
+This implimentation is the result of much discussion on the python-ideas list
+in January, 2015:
+
+https://mail.python.org/pipermail/python-ideas/2015-January/030947.html
+
+Copyright: Christopher H. Barker
+License: Apache License 2.0 http://opensource.org/licenses/apache2.0.php
+
+"""
+
+def is_close_to(actual, expected, tol=1e-8, abs_tol=0.0):
+    """
+    returns True is actual is close in value to expected. False otherwise
+
+
+    :param actual: the value that has been computed, measured, etc.
+
+    :param expected: the "known" value.
+
+    :param tol=1e-8: the relative tolerance -- it is the amount of error
+                     allowed, relative to the magnitude of the expected value.
+
+    :param abs_tol=0.0: the minimum absolute tolerance level -- useful for
+                        comparisons near zero.
+
+    NOTE: -inf, inf and NaN behave according to the IEEE 754 Standard
+
+    """
+
+    if tol < 0.0 or abs_tol < 0.0:
+        raise ValueError('error tolerances must be non-negative')
+
+    if actual == expected:  # short-circuit exact equality
+        return True
+
+    diff = abs(expected-actual)
+    return diff <= abs(tol*actual) or diff <= abs_tol
+
+
+
